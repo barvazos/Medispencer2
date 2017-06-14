32d5de040ca389a44990565913be311d12c61c5f
Amitai Handler <amitaihandler@gmail.com>
2017-06-14 14:33:59 +0300

    Cleanup

--- TestXamarinAndroid/MainActivity.cs
+++ TestXamarinAndroid/MainActivity.cs
@@ -47,63 +47,6 @@ namespace TestXamarinAndroid
             statisticsButton.Click      += (object sender, EventArgs e) => { StatisticsDisplay(); };
             perscriptionButton.Click    += (object sender, EventArgs e) => { PerscriptionDisplay(); };
 
-
-            /*
-			// Get our UI controls from the loaded layout:
-			EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
-			Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
-			Button callButton = FindViewById<Button>(Resource.Id.CallButton);
-
-			// Disable the "Call" button
-			callButton.Enabled = false;
-
-			// Add code to translate number
-			string translatedNumber = string.Empty;
-
-
-
-
-
-			translateButton.Click += (object sender, EventArgs e) =>
-			{
-                Reminder(DateTime.Now);
-
-    //            Notification.Builder builder = new Notification.Builder(this)
-    //            .SetContentTitle("Sample Notification")
-    //            .SetContentText("Hello World! This is my first notification!")
-    //            .SetSmallIcon(Resource.Drawable.abc_btn_check_material);
-
-				//// Build the notification:
-				//Notification notification = builder.Build();
-
-				//// Get the notification manager:
-				//NotificationManager notificationManager =
-				//	GetSystemService(Context.NotificationService) as NotificationManager;
-
-				//// Publish the notification:
-				//const int notificationId = 0;
-				//notificationManager.Notify(notificationId, notification);
-			};
-            */
-			/*
-			callButton.Click += (object sender, EventArgs e) =>
-			{
-				// On "Call" button click, try to dial phone number.
-				var callDialog = new AlertDialog.Builder(this);
-				callDialog.SetMessage("Call " + translatedNumber + "?");
-				callDialog.SetNeutralButton("Call", delegate
-				{
-					// Create intent to dial phone
-					var callIntent = new Intent(Intent.ActionCall);
-					callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
-					StartActivity(callIntent);
-				});
-				callDialog.SetNegativeButton("Cancel", delegate { });
-
-				// Show the alert dialog to the user and wait for response.
-				callDialog.Show();
-            };
-            */
 		}
 
 		public void Reminder(DateTime time, string title = "StamTitle", string message = "StamString")
